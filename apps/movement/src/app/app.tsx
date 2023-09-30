4c9c4acfa9b78b0d540946cbf76364f435b232f4
sheldon-st <sheldon.st@northeastern.edu>
2023-09-29 23:04:45 -0400

    style update

--- apps/movement/src/app/app.tsx
+++ apps/movement/src/app/app.tsx
@@ -23,48 +23,50 @@ export function App() {
           path="/speed"
           element={
             <div>
-              <Link to="/">Click here to go back to root page.</Link>
-              <ParameterPage name="Speed">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer imperdiet dolor et mattis dapibus. Phasellus ut
-                  maximus lectus. Pellentesque facilisis pretium nibh nec
-                  consequat. Cras placerat eleifend sagittis. Duis ligula urna,
-                  viverra nec congue sed, facilisis vel est. Proin pellentesque
-                  metus sed urna scelerisque placerat. Duis quis commodo nisi.
-                  Aenean vestibulum diam nulla, a auctor ligula pretium sit
-                  amet. Nam non convallis dui, et ultrices orci. Pellentesque
-                  vestibulum molestie imperdiet. Integer maximus tellus
-                  imperdiet porta pellentesque. Nunc feugiat urna ac ante
-                  commodo, id feugiat odio luctus. Nam venenatis et quam eu
-                  accumsan. Donec erat dolor, finibus nec malesuada id, ornare
-                  eu lorem. Quisque non posuere dui, et venenatis eros. Nunc
-                  pulvinar sed orci sed blandit. Integer viverra, sem eget
-                  pharetra facilisis, ipsum massa ullamcorper dolor, et pretium
-                  massa metus vitae eros. Phasellus posuere tortor id massa
-                  lacinia, vel tristique urna tempus. Mauris mi erat,
-                  pellentesque et facilisis non, vulputate nec lacus. In sed
-                  sapien dolor.
-                </p>
-                <p>
-                  Aenean sed velit urna. Aliquam tempus, enim eu rhoncus
-                  aliquet, enim turpis cursus sem, quis vestibulum ante risus id
-                  arcu. Proin nec augue at magna facilisis rhoncus. Integer non
-                  odio rutrum, lobortis sapien vitae, molestie est. Aliquam eget
-                  velit semper, interdum justo in, blandit tortor. Aliquam nec
-                  pretium dolor, quis porta lectus. Vestibulum urna nunc, congue
-                  ac bibendum at, egestas non erat. Donec feugiat finibus
-                  dignissim. Donec in purus et ex congue semper ut vitae sapien.
-                  Nulla venenatis enim quis congue iaculis. Praesent laoreet
-                  ullamcorper diam. Vestibulum porttitor bibendum ipsum vitae
-                  semper.lley of type and scrambled it to make a type specimen
-                  book. It has survived not only five centuries, but also the
-                  leap into electronic typesetting, remaining essentially
-                  unchanged. It was popularised in the 1960s with the release of
-                  Letraset sheets containing Lorem Ipsum passages, and more
-                  recently with desktop publishing software like Aldus PageMaker
-                  including versions of Lorem Ipsum.
-                </p>
+              <ParameterPage name="Speed" previous="Fluidity" next="Path">
+                <div className="info">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer imperdiet dolor et mattis dapibus. Phasellus ut
+                    maximus lectus. Pellentesque facilisis pretium nibh nec
+                    consequat. Cras placerat eleifend sagittis. Duis ligula
+                    urna, viverra nec congue sed, facilisis vel est. Proin
+                    pellentesque metus sed urna scelerisque placerat. Duis quis
+                    commodo nisi. Aenean vestibulum diam nulla, a auctor ligula
+                    pretium sit amet. Nam non convallis dui, et ultrices orci.
+                    Pellentesque vestibulum molestie imperdiet. Integer maximus
+                    tellus imperdiet porta pellentesque. Nunc feugiat urna ac
+                    ante commodo, id feugiat odio luctus. Nam venenatis et quam
+                    eu accumsan. Donec erat dolor, finibus nec malesuada id,
+                    ornare eu lorem. Quisque non posuere dui, et venenatis eros.
+                    Nunc pulvinar sed orci sed blandit. Integer viverra, sem
+                    eget pharetra facilisis, ipsum massa ullamcorper dolor, et
+                    pretium massa metus vitae eros. Phasellus posuere tortor id
+                    massa lacinia, vel tristique urna tempus. Mauris mi erat,
+                    pellentesque et facilisis non, vulputate nec lacus. In sed
+                    sapien dolor.
+                  </p>
+                  <p>
+                    Aenean sed velit urna. Aliquam tempus, enim eu rhoncus
+                    aliquet, enim turpis cursus sem, quis vestibulum ante risus
+                    id arcu. Proin nec augue at magna facilisis rhoncus. Integer
+                    non odio rutrum, lobortis sapien vitae, molestie est.
+                    Aliquam eget velit semper, interdum justo in, blandit
+                    tortor. Aliquam nec pretium dolor, quis porta lectus.
+                    Vestibulum urna nunc, congue ac bibendum at, egestas non
+                    erat. Donec feugiat finibus dignissim. Donec in purus et ex
+                    congue semper ut vitae sapien. Nulla venenatis enim quis
+                    congue iaculis. Praesent laoreet ullamcorper diam.
+                    Vestibulum porttitor bibendum ipsum vitae semper.lley of
+                    type and scrambled it to make a type specimen book. It has
+                    survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It
+                    was popularised in the 1960s with the release of Letraset
+                    sheets containing Lorem Ipsum passages, and more recently
+                    with desktop publishing software like Aldus PageMaker
+                    including versions of Lorem Ipsum.
+                  </p>
+                </div>
                 <MovementVideo
                   videoSrc="https://uibyme.blob.core.windows.net/movement/Timeline 1.mp4"
                   title="Speed"
--- apps/movement/src/components/ParameterPage.tsx
+++ apps/movement/src/components/ParameterPage.tsx
@@ -5,15 +5,49 @@ import { MovementVideo } from './video';
 interface IParameterPageProps {
   name: string;
   children: React.ReactNode;
+  previous: string;
+  next: string;
 }
 
-export const ParameterPage: FC<IParameterPageProps> = ({ name, children }) => {
+export const ParameterPage: FC<IParameterPageProps> = ({
+  name,
+  children,
+  previous,
+  next,
+}) => {
   return (
     <div>
-      <h1 className="h1">
-        <span className="span">Parameter: </span>
-        {name}
-      </h1>
+      <div
+        className="header"
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: '0 ',
+          height: 'fit-content',
+          alignItems: 'center',
+        }}
+      >
+        <h1 className="h1">
+          <Link to="/" style={{ textDecoration: 'none', textAlign: 'center' }}>
+            <span className="ret">← </span>
+          </Link>
+          <span className="span">Parameter: </span>
+          {name}
+        </h1>
+        <div className="button-container">
+          <Link to={`/${previous}`} style={{ textDecoration: 'none' }}>
+            {/* back arrow character and then previous parameter in span not button*/}
+            <span className="link">← {previous}</span>
+          </Link>
+          <div style={{ width: '16px' }}></div>
+          <Link to={`/${next}`} style={{ textDecoration: 'none' }}>
+            <span className="link">{next} →</span>
+          </Link>
+        </div>
+      </div>
+      {/* Buttons for return home and next parameter: */}
+
       <p></p>
       <div>{children}</div>
     </div>
